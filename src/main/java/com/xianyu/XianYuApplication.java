1b3adfc993956793a4aeea9eb0b915987536b65b
poooooob <thcking1217@gmail.com>
2024-03-23 15:46:58 +0800

    大学生二手交易平台初始创建代码

---
+++ src/main/java/com/xianyu/XianYuApplication.java
@@ -0,0 +1,13 @@
+package com.xianyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XianYuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XianYuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xianyu/XianYuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xianyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XianYuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
